1c36de7e8986b8df71288449a8e43ca80ad16fc6
sh-aams <139136259+sh-aams@users.noreply.github.com>
2025-03-06 16:41:51 +0200

    Add files via upload

---
+++ camera_pins.h
@@ -0,0 +1,16 @@
+#define RESET_GPIO_NUM  -1  
+#define Y3_GPIO_NUM      9 
+#define Y5_GPIO_NUM      10 
+#define Y7_GPIO_NUM      18
+#define Y9_GPIO_NUM      16      
+#define PCLK_GPIO_NUM    13    
+#define VSYNC_GPIO_NUM   6  
+#define SIOC_GPIO_NUM    5  
+#define PWDN_GPIO_NUM    -1
+#define Y2_GPIO_NUM      11  
+#define Y4_GPIO_NUM      8      
+#define Y6_GPIO_NUM      12    
+#define Y8_GPIO_NUM      17   
+#define XCLK_GPIO_NUM    -1  //-1 Only if using a breakout that provides an internal camera clock
+#define HREF_GPIO_NUM    7 
+#define SIOD_GPIO_NUM    4
